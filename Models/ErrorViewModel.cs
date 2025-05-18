0eaa1d87dabda6415d27c4eb3574bce103af7c77
Suhas Gaikwad <Suhas.Gaikwad@tietoevry.com>
2025-05-18 12:03:18 +0530

    Code added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearningDevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
